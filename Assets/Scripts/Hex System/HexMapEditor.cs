cd57e50e5eb5dcbb829c71bbee51ebe898e87c97
SushiMaster09 <sushimaster679@gmail.com>
2026-03-08 23:12:05 +0000

    the hex system in its partial entirety

---
+++ Assets/Scripts/Hex System/HexMapEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour 
+{
+	public Color[] colors;
+
+	public HexGrid hexGrid;
+
+	private Color activeColor;
+
+	private int activeElevation;
+
+    private void Awake() 
+	{
+		SelectColor(0);
+	}
+
+	private void Update() 
+	{
+		if (Input.GetMouseButton(0) &&!EventSystem.current.IsPointerOverGameObject()) 
+		{
+			HandleInput();
+		}
+	}
+
+	private void HandleInput() 
+	{
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(inputRay, out hit)) 
+		{
+            EditCell(hexGrid.GetCell(hit.point));
+		}
+	}
+
+    private void EditCell(HexCell cell)
+    {
+        cell.color = activeColor;
+		cell.Elevation = activeElevation;
+        hexGrid.Refresh();
+    }
+
+    public void SelectColor(int index) 
+	{
+		activeColor = colors[index];
+	}
+
+    public void SetElevation(float elevation)
+    {
+        activeElevation = (int)elevation;
+    }
+}
